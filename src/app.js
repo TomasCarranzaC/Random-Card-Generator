e1610ec8cec142292ddd0f424edbdf621ccc8b91
TomasCarranzaC <tomascarranzac@gmail.com>
2022-06-15 19:03:20 +0000

    Random Card Generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,55 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".top-suit").innerHTML = symbol;
+  document.querySelector(".number").innerHTML = text;
+  document.querySelector(".bottom-suit").innerHTML = symbol;
 };
+
+//autoreload a 10 segundos
+window.setTimeout(function() {
+  window.location.reload();
+}, 10000);
+
+//numeros o letras
+function generateRandomNumber() {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+}
+
+//escoger palo
+let generateRandomSuit = () => {
+  let suit = ["♥", "♦", "♠", "♣"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+};
+
+//guardado
+var text = generateRandomNumber();
+var symbol = generateRandomSuit();
+
+//Condicional para el color de las cartas rojas
+if (symbol == "♦" || symbol == "♥") {
+  const colorTop = document.querySelector(".top-suit").style;
+  const colorNumber = document.querySelector(".number").style;
+  const colorBottom = document.querySelector(".bottom-suit").style;
+  colorTop.color = "red";
+  colorNumber.color = "red";
+  colorBottom.color = "red";
+}
